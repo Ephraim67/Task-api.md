a9863dcca31713b825d584c2348692e22ca43026
Ephraim67 <jonathaneuphrem@gmail.com>
2025-06-21 15:24:04 +0100

    Corrected invalid errror

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 // const User = require('../models/user');
 const Student = require('../models/students');
+const User = require('../models/admin');
 
 const authenticateUser = async (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
